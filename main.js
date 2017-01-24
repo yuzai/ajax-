76bbb59113df6b32bd6fe62c23fe4edcf345e308
yuzai <a545905403@163.com>
2017-01-24 19:25:53 +0800

    增加dom操作，读取form内容，回调函数简单进行输出

--- main.js
+++ main.js
@@ -1,13 +1,34 @@
-var ajax = new Ajax({
-  method:'post',
-  url:'http://localhost:3000',
-  callback:function(res){
-    console.log(res)
-  },
-  data: {
-    name:'xiaobo',
-    age:'21',
-    password:'123456'
-  }
-});
-ajax.send();
+var button = document.getElementById('test');
+var button2 = document.getElementById('test2');
+var account = document.getElementById('account');
+var password = document.getElementById('password');
+button.onclick = function(){
+  var data = {
+    'account':account.value,
+    'password':password.value
+  };
+  var ajax = new Ajax({
+    method:'get',
+    url:'http://localhost:3000',
+    callback:function(res){
+      alert(res)
+    },
+    data: data
+  });
+  ajax.send();
+}
+button2.onclick = function(){
+  var data = {
+    'account':account.value,
+    'password':password.value
+  };
+  var ajax = new Ajax({
+    method:'post',
+    url:'http://localhost:3000',
+    callback:function(res){
+      alert(res)
+    },
+    data: data
+  });
+  ajax.send();
+}
